d72ab34b75be7c59730002a22f9260636cb6ccd4
dhawoodhkhan <ddhawoodhkhan00@gmail.com>
2023-06-25 14:51:16 +0530

    backend 25-06-2023

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:['http://localhost:3000','https://dhawoodhkhan.github.io/']}));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 const path = require('path');
